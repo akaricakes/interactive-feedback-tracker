6b842d2baa7b0e09db0011287711aaa1245ebb5f
akaricakes <happypooh0928@gmail.com>
2026-05-10 12:59:56 -0400

    Adding all edited files.

---
+++ interactive-feedback-tracker/src/components/Form.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import Statistics from './Statistics'
+
+type Feedback = {
+    feedbackType: string
+    name: string
+    comment: string
+    rating: string
+    otherFeedback?: string
+}
+
+const Form = () => {
+    const [formData, setFormData] = useState<Feedback>({
+        feedbackType: '',
+        name: '',
+        comment: '',
+        rating: ''
+    })
+
+    const [allFeedback, setAllFeedback] = useState<Feedback[]>([])
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setFormData({...formData, [name]: value})
+    }
+
+    useEffect(() => {
+        localStorage.removeItem("feedbacks");
+        setAllFeedback([]);
+    }, []);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        const newFeedback = [...allFeedback, formData]
+        setAllFeedback(newFeedback)
+        localStorage.setItem("feedbacks", JSON.stringify([...allFeedback, formData]))
+
+        setFormData({
+            feedbackType: '',
+            name: '',
+            comment: '',
+            rating: ''
+        })
+    }
+
+    type GroupedFeedback = {
+        [key: string]: {
+            totalRating: number
+            count: number
+            averageRating?: number
+        }
+    }
+
+    const groupFeedbackType = (): GroupedFeedback => {
+        const groupFeedback: GroupedFeedback = {}
+        allFeedback.forEach((feedback)=>{
+            const type = feedback.feedbackType
+            if(!groupFeedback[type])
+            {
+                groupFeedback[type] = {totalRating: 0, count: 0}
+            }
+            groupFeedback[type].totalRating += Number(feedback.rating)
+            groupFeedback[type].count += 1
+        })
+
+        Object.keys(groupFeedback).forEach((key) => {
+        groupFeedback[key].averageRating =
+            groupFeedback[key].totalRating / groupFeedback[key].count
+        })
+
+        return groupFeedback;
+    }
+
+    const groupFeedback = groupFeedbackType()
+    
+    return (
+        <div className="container mx-auto mt-5 rounded-md bg-white shadow-lg flex items-center flex-col justify-center">
+            <form onSubmit={handleSubmit} className="p-10 md:p-0 flex flex-col w-full max-w-xl gap-4 items-center">
+                <h1 className="mt-4 text-4xl font-bold capitalize">Tech Assistance Feedback Form</h1>
+                <select value={formData.feedbackType} onChange={handleChange} name="feedbackType" required className="w-full bg-gray-100 px-4 py-2 rounded-md border border-gray-400">
+                    <option value="">Select Feedback Type*</option>
+                    <option value="Hardware Issues">Hardware Issues</option>
+                    <option value="Device Connectivity Issues">Device Connectivity Issues</option>
+                    <option value="Printer Ink Request">Printer Ink Request</option>
+                    <option value="Other">Other (please specify)</option>
+                </select>
+                {formData.feedbackType === "Other" && (
+                    <input type="text" name="otherFeedback" onChange={handleChange} placeholder="Please specify...*" required className="w-full bg-gray-100 px-4 py-2 rounded-md border border-gray-400"/>
+                )}
+                <input onChange={handleChange} type="text" name="name" required className="w-full bg-gray-100 px-4 py-2 rounded-md border border-gray-400" placeholder="Enter your name...*"></input>
+                <select onChange={handleChange} value={formData.rating} name="rating" required className="w-full bg-gray-100 px-4 py-2 rounded-md border border-gray-400">
+                    <option value="">Rate Us*</option>
+                    <option value="1">1. Poor</option>
+                    <option value="2">2. Fair</option>
+                    <option value="3">3. Good</option>
+                    <option value="4">4. Great</option>
+                    <option value="5">5. Excellent</option>
+                </select>
+                <textarea onChange={handleChange} value={formData.comment} name="comment" rows={4} className="w-full bg-gray-100 px-4 py-2 rounded-md border border-gray-400" placeholder="Please drop your valuable feedback!"></textarea>
+                <h1><span className="text-red-500">*</span> is required.</h1>
+                <button className="px-6 mt-4 mb-4 w-full py-2 bg-[#BA6B57] hover:bg-[#E0A387] rounded-md border-none outline-none text-white">Submit</button>
+            </form>
+
+            {allFeedback.length > 0 && (
+                <Statistics
+                    allFeedback={allFeedback}
+                    groupFeedback={groupFeedback}
+                />
+            )}
+        </div>
+    );
+}
+
+export default Form
---
+++ interactive-feedback-tracker/src/components/Statistics.tsx
@@ -0,0 +1,50 @@
+type Props = {
+  allFeedback: any[]
+  groupFeedback: any
+}
+
+const Statistics = ({ allFeedback, groupFeedback }: Props) => {
+    return (
+        <>
+            <div className="w-full lg:w-[70%] bg-[#E0A387] p-5 shadow-md">
+                <h1 className="text-4xl text-white text-center mb-4">Total Feedback: {allFeedback.length}</h1>
+                <div className="w-full flex items-center gap-4 mb-5 rounded-md justify-center">
+                {
+                    Object.keys(groupFeedback).length > 0 ? (
+                        Object.keys(groupFeedback).map((type) => {
+                            const averageRating = (
+                                groupFeedback[type].totalRating/groupFeedback[type].count
+                            )
+                            return (
+                                <div className="card p-6 w-72 rounded-md bg-white shadow-md">
+                                    <h3 className="font-bold text-xl">{type}</h3>
+                                    <h4>Average Rating: <span>{averageRating.toFixed(1)}</span></h4>
+                                    <h4>Total Feedback: <span>{groupFeedback[type].count}</span></h4>
+                                </div>
+                            )
+                        })
+                    ) : ""
+                }
+                </div>
+                {
+                    allFeedback.map((feedback: any, index: number) => {
+                        return (
+                            <div key = {index} className = "flex p-4 rounded-md bg-white shadow-md flex-col items-start justify-between mb-5">
+                                <h2><span className="font-bold">Feedback Type: </span>{feedback.feedbackType}</h2>
+                                {feedback.feedbackType === "Other" && (
+                                    <h2><span className="font-bold">Other (please specify): </span>{feedback.otherFeedback}</h2>
+                                )}
+                                <h2><span className="font-bold">Name: </span>{feedback.name}</h2>
+                                <h2><span className="font-bold">Rating: </span>{parseFloat(feedback.rating).toFixed(1)}</h2>
+                                <h2><span className="font-bold">Comment: </span>{feedback.comment}</h2>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
+
+
+export default Statistics
